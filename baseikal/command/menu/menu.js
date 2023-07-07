bc719d4f55368ce442535a6908e5f92c7dfe878b
yogabot01 <yogabaatiar1@gmail.com>
2023-07-07 14:15:55 +0700

    yoga

---
+++ baseikal/command/menu/menu.js
@@ -0,0 +1,51 @@
+const menu =  ` 
+│🎭 Owner  : VynaChan 
+│🎭 Version : 1,0
+│🎭 Baileys : 4.4.0
+
+[ YT: https://youtube.com/@VynaaChan ]
+ ▰▱▰▱▰▱▰▱▰▱▰▱▰▱
+ ╔─═─═⊱ *「 *BUG MENU* 」 ─═─═⬣
+│┏⊱
+║⿻ Bugmenu
+║┗⊱
+┗━━━━━━━━━━━━━━━━━━⬣
+ ╔─═─═⊱ *「 *ALLMENU* 」 ─═─═⬣
+│┏⊱
+║⿻ Allmenu
+║┗⊱
+┗━━━━━━━━━━━━━━━━━━⬣
+ ╔─═─═⊱ *「 *MENU MODS* 」 ─═─═⬣
+│┏⊱
+║⿻ Sewa
+║⿻ Logo
+║⿻ Textmaker
+║⿻ Belajarkita
+║⿻ Methodeband
+║⿻ Listjualan
+║⿻ Listowner
+║⿻ Listwibu
+║⿻ Nowa 628xxx
+║⿻ Verif 62xxxxx
+║⿻ Ban add 628xx
+║⿻ User add 62xx
+║⿻ User del 62xxx
+║┗⊱
+┗━━━━━━━━━━━━━━━━━━⬣
+╔─═─═⊱ *「 *MENU GROUP* 」 ─═─═⬣
+│┏⊱
+║⿻ Antilink on / off
+║⿻ Welcome on / off
+║⿻ Detectadmin on / off
+║⿻ Kick 628xxx
+║⿻ Add 628xxx
+║⿻ Promote 628xx
+║⿻ Demote 628xx
+║┗⊱
+┗━━━━━━━━━━━━━━━━━━⬣
+┏━━⬣  Thanks To  友
+┃ 🔥 Allah SWT
+┃ 🔥 vynaa
+┃ 🔥 Xwby
+┗━━⬣  ⿻ VynaBotz ⿻`
+exports.menu = menu
